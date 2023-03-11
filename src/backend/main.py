c4dbaf5ef58e88907487f03660d9d4df23669794
Bartosz Swedziol <bjs889@cs.bham.ac.uk>
2023-03-11 20:55:14 +0000

    change order for route matching

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,17 +3,7 @@ from time import sleep
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-
 pump = Motor(forward=26, backward=20)
-
-@app.get("/watering/start/{time}")
-async def start_watering(time):
-    time = int(time)
-    if (time >= 30):
-        return HTTPException(status_code=400, detail="Time too high")
-    pump.forward(1)
-    sleep(time)
-    pump.stop()
 
 @app.get("/watering/start/fast")
 async def start_watering_fast():
@@ -25,6 +15,15 @@ async def start_watering_fast():
 async def start_watering_slow():
     pump.forward(0.6)
     sleep(5)
+    pump.stop()
+
+@app.get("/watering/start/{time}")
+async def start_watering(time):
+    time = int(time)
+    if (time >= 30):
+        return HTTPException(status_code=400, detail="Time too high")
+    pump.forward(1)
+    sleep(time)
     pump.stop()
 
 @app.get("watering/stop")
