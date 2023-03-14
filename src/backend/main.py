ef7b5cd511da59b12914aba54f1583c6e68e6284
Bartosz Swedziol <bartosz.swedziol@gmail.com>
2023-03-14 22:04:21 +0000

    Add watering control

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,9 +1,14 @@
 from gpiozero import Motor
 from time import sleep
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 pump = Motor(forward=26, backward=20)
+
+class WateringConfig(BaseModel):
+    duration: int
+    speed: float
 
 @app.get("/watering/start/fast")
 async def start_watering_fast():
@@ -17,13 +22,16 @@ async def start_watering_slow():
     sleep(5)
     pump.stop()
 
-@app.get("/watering/start/{time}")
-async def start_watering(time):
-    time = int(time)
-    if (time >= 30):
-        return HTTPException(status_code=400, detail="Time too high")
-    pump.forward(1)
-    sleep(time)
+@app.post("/watering/start")
+async def start_watering(config: WateringConfig):
+    if (config.duration > 30):
+        return HTTPException(status_code=400, detail="Duration too long")
+
+    if (config.speed > 1 or config.speed < 0):
+        return HTTPException(status_code=400, detail="Incorrect speed")
+
+    pump.forward(config.speed)
+    sleep(config.duration)
     pump.stop()
 
 @app.get("watering/stop")
