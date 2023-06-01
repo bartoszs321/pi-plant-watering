3149cee77c120894a5d57cd9a5613a25870b7860
Bartosz Swedziol <bartosz.swedziol@gmail.com>
2023-06-01 18:48:21 +0100

    toast when login failed

--- src/frontend/pi-plant-watering/src/Components/Login/LoginForm.jsx
+++ src/frontend/pi-plant-watering/src/Components/Login/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../utils/auth-context";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const LoginForm = () => {
   const [username, setUsername] = useState();
@@ -8,6 +10,8 @@ const LoginForm = () => {
   const navigate = useNavigate();
 
   const [auth, setAuth] = useContext(AuthContext);
+
+  const notify = () => toast("Wow so easy!");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -24,30 +28,54 @@ const LoginForm = () => {
       formBody.push(encodedKey + "=" + encodedValue);
     }
     formBody = formBody.join("&");
-    const response = await fetch(process.env.REACT_APP_BACKEND_ADDRESS + `/token`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: formBody,
-    });
-    const data = await response.json();
-    if (response.ok) {
-      setAuth({
-        state: {
-          isFetching: false,
-          isExpired: false,
-          expires: data.expires,
+    var response;
+    try {
+      response = await fetch(process.env.REACT_APP_BACKEND_ADDRESS + `/token`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
         },
-        user: {
-          username: username,
-          access_token: data.access_token,
-          isAuthenticated: true,
-        },
+        body: formBody,
       });
-      navigate("/");
-    } else {
-      throw response.statusText;
+      const data = await response.json();
+      if (response.ok) {
+        setAuth({
+          state: {
+            isFetching: false,
+            isExpired: false,
+            expires: data.expires,
+          },
+          user: {
+            username: username,
+            access_token: data.access_token,
+            isAuthenticated: true,
+          },
+        });
+        navigate("/");
+      } else {
+        toast.error(`🦄 ${data.detail}`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+        return response.statusText;
+      }
+    } catch (e) {
+      toast.error(`🦄 ${e.message}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
     }
   };
 
@@ -60,29 +88,43 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="controls">
-        <div className="control">
-          <label>Username</label>
-          <input
-            type="text"
-            value={username}
-            onChange={usernameChangeHandler}
-          />
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="controls">
+          <div className="control">
+            <label>Username</label>
+            <input
+              type="text"
+              value={username}
+              onChange={usernameChangeHandler}
+            />
+          </div>
+          <div className="control">
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={passwordChangeHandler}
+            />
+          </div>
         </div>
-        <div className="control">
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={passwordChangeHandler}
-          />
+        <div className="customWatering">
+          <button type="submit">Login</button>
         </div>
-      </div>
-      <div className="customWatering">
-        <button type="submit">Login</button>
-      </div>
-    </form>
+      </form>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
   );
 };
 
