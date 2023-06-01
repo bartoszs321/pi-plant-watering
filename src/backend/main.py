11f4dd7b9a91a7950527ae211390eed15a8f3183
Bartosz Swedziol <bartosz.swedziol@gmail.com>
2023-06-01 18:48:42 +0100

    add vpn cors

--- src/backend/main.py
+++ src/backend/main.py
@@ -55,7 +55,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000', 'http://192.168.0.55:3000'],
+    allow_origins=['http://localhost:3000', 'http://192.168.0.55:3000', 'http://172.29.4.222:3000'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
